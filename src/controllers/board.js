ea2892497450c4b2e6931ac5411f00e51968eb1f
Svetlana <gubinas83@gmail.com>
2020-04-16 20:02:30 +0300

    Add task sort

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -4,7 +4,7 @@ import {Position, render} from '../utils.js';
 import Task from '../components/task.js';
 import TaskEdit from '../components/task-edit.js';
 import NoTasks from '../components/no-task.js';
-// import Sort from '../components/sort.js';
+import Sort from '../components/sort.js';
 
 export default class BoardController {
   constructor(container, tasks) {
@@ -13,20 +13,22 @@ export default class BoardController {
     this._board = new Board();
     this._taskList = new BoardTasks();
     this._noTasks = new NoTasks();
-    // this._sort = new Sort();
+    this._sort = new Sort();
+    this._shownTasks = [];
   }
 
   init() {
     render(this._container, this._board.getElement(), Position.BEFOREEND);
+    render(this._board.getElement(), this._sort.getElement(), Position.AFTERBEGIN);
     render(this._board.getElement(), this._taskList.getElement(), Position.BEFOREEND);
-
-    let shownTasks = this._tasks.splice(0, 8);
-
-    if (shownTasks.length) {
-      shownTasks.forEach((taskMock) => this._renderTask(taskMock));
+    const newShowTasks = this._tasks.splice(0, 8);
+    if (newShowTasks.length) {
+      newShowTasks.forEach((taskMock) => this._renderTask(taskMock));
     } else {
       render(this._board.getElement(), this._noTasks.getElement(), Position.AFTERBEGIN);
     }
+    this._shownTasks = this._shownTasks.concat(newShowTasks);
+    this._sort.getElement().addEventListener(`click`, (evt) => this._onSortLinkClick(evt));
   }
 
   _renderTask(task) {
@@ -65,5 +67,29 @@ export default class BoardController {
       });
 
     render(this._taskList.getElement(), taskComponent.getElement(), Position.BEFOREEND);
+  }
+
+  _onSortLinkClick(evt) {
+    evt.preventDefault();
+
+    if (evt.target.tagName !== `A`) {
+      return;
+    }
+
+    this._taskList.getElement().innerHTML = ``;
+
+    switch (evt.target.dataset.sortType) {
+      case `date-up`:
+        const sortedByDateUpTasks = this._shownTasks.slice().sort((a, b) => a.dueDate - b.dueDate);
+        sortedByDateUpTasks.forEach((taskMock) => this._renderTask(taskMock));
+        break;
+      case `date-down`:
+        const sortedByDateDownTasks = this._shownTasks.slice().sort((a, b) => b.dueDate - a.dueDate);
+        sortedByDateDownTasks.forEach((taskMock) => this._renderTask(taskMock));
+        break;
+      case `default`:
+        this._shownTasks.forEach((taskMock) => this._renderTask(taskMock));
+        break;
+    }
   }
 }
